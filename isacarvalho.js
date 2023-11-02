e5008778293a9c71552732bab98da5877fb78156
Isa Carvalho <isadecarvalhodev@gmail.com>
2023-11-02 05:07:10 +0000

    last request

--- isacarvalho.js
+++ isacarvalho.js
@@ -1,21 +1,26 @@
-class Bookshelf {
-  constructor() {
-    this.favoriteBooks = [];
+function Bookshelf(ownerName,favoriteBooks) {
+  this.ownerName = "";  
+  this.favoriteBooks = [];
   }
 
-  addFavoriteBook(bookName) {
+  Bookshelf.prototype.addFavoriteBook = function(bookName) {
     if (!bookName.includes("Great")) {
       this.favoriteBooks.push(bookName);
     }
-  }
+  };
 
-  printFavoriteBooks() {
-    console.log(`Favorite Books: ${String(this.favoriteBooks.length)}`);
+  Bookshelf.prototype.printFavoriteBooks = function() {
+    console.log(`${String(this.ownerName)} Favorite Books: ${String(this.favoriteBooks.length)}`);
     for (let bookName of this.favoriteBooks) {
       console.log(bookName);
     }
+  };
+
+  Bookshelf.prototype.sortFavoriteBooksByAlphabeticDescendingOrder = function() {
+    this.favoriteBooks = this.favoriteBooks.sort((a,b) => b.localeCompare(a));
+    console.log(this.favoriteBooks);
   }
-}
+
 
 function loadBooks(bookshelf) {
   fakeAjax(BOOK_API, function onBooks(bookNames) {
@@ -23,12 +28,14 @@ function loadBooks(bookshelf) {
       bookshelf.addFavoriteBook(bookName);
     }
     bookshelf.printFavoriteBooks();
+    bookshelf.sortFavoriteBooksByAlphabeticDescendingOrder();
   });
 }
 
 var BOOK_API = "https://some.url/api";
-var myBooks = new Bookshelf();
+var myBooks = new Bookshelf("Lisa ");
 loadBooks(myBooks);
+
 
 // ***********************
 
